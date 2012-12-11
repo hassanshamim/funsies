27ba0058b4b8860bf0450a61a4c002f4d912ff7e
hassanshamim <hassan.has.you@gmail.com>
2012-12-11 15:58:29 -0600

    now 100x faster!

--- dailyprogrammer/challenge-114.rb
+++ dailyprogrammer/challenge-114.rb
@@ -11,10 +11,37 @@ def levenshtein(a, b)
   end
 end
 
+def variations2(word, dict)
+  result = []
+  word.length.times do |i|
+  #   puts 'GOT HERE'
+    ('a'..'z').each do |letter|
+      if letter != word[i]
+        s = word[0..i] + letter + word[(i+1)..-1]
+  #     puts "considering: #{s}"
+        result << s if dict.include? s
+      end
+    end
+  end
+end
+
+def variations1(word, dict)
+  result = []
+  word.length.times do |i|
+    ('a'..'z').each do |letter|
+      s = word[0..i] + letter + word[(i+1)..-1]
+      s = word.dup
+      s[i]= letter
+      result << s if dict.include? s
+    end
+  end
+  result.delete( word )
+  result
+end
 
 def ladder(word1, word2)
   doc = File.open('selected_four-letter_words.txt')
-  words = doc.readlines.map(&:chomp)
+  words = doc.readlines.map{ |line| line.chomp }
   doc.close
   build_bridge(word1, word2, words)
 end
@@ -26,12 +53,12 @@ def build_bridge( seed, target, dict )
   #breadth first search, goes until it finds target word
   until current == target
     puts "working on word #{current}"
-    neighbors = dict.select{ |entry| levenshtein( current, entry ) == 1 }
-    hash_table[current] = neighbors
+    ## neighbors = dict.select{ |entry| levenshtein( current, entry ) == 1 }
+    neighbors = variations1(current, dict)
+    hash_table[current] = neighbors - hash_table.keys #not unidirected w/o this line
     queue += neighbors - hash_table.keys
     current = queue.shift while hash_table.has_key?( current ) #haven't tested this way
   end
-
   bridge = [target]
 
   until bridge.last == seed
@@ -40,5 +67,18 @@ def build_bridge( seed, target, dict )
     end
   end
 
-  puts bridge.reverse
+  puts "WORD BRIDGE", bridge.reverse
 end
+
+=begin
+require '~/rails/funsies/dailyprogrammer/challenge-114.rb'
+require 'benchmark'
+doc = File.open('selected_four-letter_words.txt')
+words = doc.readlines.map{ |line| line.chomp }
+doc.close
+Benchmark.bm do |x|
+  x.report { variations1('back', words) }  
+  x.report { variations2('back', words) }  
+  x.report { words.select{|e| levenshtein('back', e) == 1 } }  
+end
+=end
